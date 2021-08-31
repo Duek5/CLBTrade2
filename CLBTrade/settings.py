9ca11cffebe275b971dcbe6f411ffda7118ebf10
Duek5 <88674000+Duek5@users.noreply.github.com>
2021-08-31 22:25:51 +0300

    Update settings.py

--- CLBTrade/settings.py
+++ CLBTrade/settings.py
@@ -14,34 +14,10 @@ import django_heroku
 import os
 from pathlib import Path
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
